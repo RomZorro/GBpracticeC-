ab5be51a28157d5c4c7bee6551d5ad71f009949b
roman zotov <mr_monkey@inbox.ru>
2022-12-02 16:08:35 +0300

    3 task updated

--- HW1.6/Program.cs
+++ HW1.6/Program.cs
@@ -4,6 +4,13 @@
 4 -> да
 -3 -> нет
 7 -> нет */
+/* второе решение
+Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int n = Convert.ToInt32(s);
+Console.WriteLine(n % 2 == 0 ? "Четное" : "Не четное");
+*/
+
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a % 2 == 1)
@@ -14,5 +21,6 @@ else
 {
     Console.WriteLine("Число " + a + " является чётным ");
 }
+
 
 
