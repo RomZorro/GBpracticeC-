c8adff8ae6c06c750bea1737c85d1b9cb439e437
roman zotov <mr_monkey@inbox.ru>
2022-12-06 15:30:54 +0300

    Task 2.10 is done

---
+++ HW2.10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+if (num > 999)
+{
+System.Console.WriteLine("Число не трехзначное");
+}
+else
+{
+string chr = Convert.ToString(num);
+System.Console.WriteLine($"{chr[1]}");
+}
