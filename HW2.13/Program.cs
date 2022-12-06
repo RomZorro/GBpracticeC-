d63e098accecbe6e16de777c88c816232f29025a
roman zotov <mr_monkey@inbox.ru>
2022-12-06 16:49:53 +0300

    Task 2.13 is done

---
+++ HW2.13/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+string chr = Convert.ToString(num);
+
+if (chr.Length >= 3)
+{
+    System.Console.WriteLine($"{chr[2]}");
+}
+else
+{
+    System.Console.WriteLine("Число не содержит третьего знака");
+}
